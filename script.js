6cfce0c07ca0bd17717591608e00df79653a8046
kesia almeida <kesia.alves@ymail.com>
2023-12-20 11:12:30 -0300

    estilização css

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// function selectGear(gear) {
+//     document.getElementById('gear').value = gear;
+// }
 function selectGear(gear) {
-    document.getElementById('gear').value = gear;
+  resetButtonColors();
+
+  // Selecione o botão atual e defina a cor para verde
+  let currentButton = document.querySelector(".button" + gear);
+  currentButton.style.backgroundColor = "green";
+
+  // Seu código existente aqui
+}
+
+function resetButtonColors() {
+  // Reinicie a cor de todos os botões para a cor padrão
+  var buttons = document.querySelectorAll(".button");
+  buttons.forEach(function(button) {
+    button.style.backgroundColor = ""; // Define a cor para a padrão (transparente)
+  });
 }
